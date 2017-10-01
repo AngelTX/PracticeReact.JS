377aeed32748ff3f0dbfb94780a59c31001d2cdb
Miguel Bernal <miguel.bernal@g.austincc.edu>
2017-09-30 20:48:14 -0500

    Created and imported header, footer, and home routes.

--- nba/src/App.js
+++ nba/src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//COMPONENTS
+import Header from './header';
+import Footer from './footer';
+import Home from './home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>Hello world</div>
     );
   }
 }
