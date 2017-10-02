b845df91a093409759ae8f978c325907d1943e0a
Miguel Bernal <miguel.bernal@g.austincc.edu>
2017-10-01 19:30:28 -0500

    Added styling to blocks. Also fixed some EJS templating errors within the Blocks component.

--- nba/src/components/blocks.js
+++ nba/src/components/blocks.js
@@ -1,35 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Reveal from 'react-reveal';
-import 'animate.css/animate.css';
+import Reveal from 'react-reveal'; // REACT REVEAL
+import 'animate.css/animate.css'; //  REACT REVEAL
 
-const generateBlocks = ({blocks}) =>{
-  if(blocks){
-    return blocks.map((item)=>{
-      return(
-        <Reveal key={item.id}effect="animated fadeInUp" className={'item ${item.type}'}>
-          <div className="veil"></div>
-        <div className="image" style={{background:`url(/images/blocks/${item.image}) no-repeat`}}>
+const generateBlocks = ({blocks}) => {
 
-        </div>
-        <div className="title">
-          <Link to={item.link}>{item.title}</Link>
-        </div>
+    if (blocks) {
+        return blocks.map((item) => {
+            return (
+                <Reveal key={item.id} effect="animated fadeInUp" className={`item ${item.type}`}>
+                    <div className="veil"></div>
+                    <div
+                        className="image"
+                        style={{
+                        background: `url(/images/blocks/${item.image}) no-repeat`}}>
 
-        </Reveal>
-      )
-    })
-  }
+                    </div>
+                    <div className="title">
+                        <Link to={item.link}>{item.title}</Link>
+                    </div>
+                </Reveal>
+            )
+        })
+    }
 }
-const Blocks = (props) =>{
 
-
-
-  return(
-    <div className="home_blocks">
-      {generateBlocks(props)}
-    </div>
-  )
+const Blocks = (props) => {
+    return (
+        <div className="home_block">
+            {generateBlocks(props)}
+        </div>
+    )
 }
 
 export default Blocks;
